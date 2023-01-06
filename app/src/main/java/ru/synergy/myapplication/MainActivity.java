71dc726e5b64b1d837cfdd096e28a72f1e300091
bystrovdev <bystrovdev@mail.ru>
2023-01-06 17:37:11 +0400

    Added Java screen configuration example with Constraint Layout

--- app/src/main/java/ru/synergy/myapplication/MainActivity.java
+++ app/src/main/java/ru/synergy/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package ru.synergy.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 import android.view.View;
@@ -14,12 +15,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
 
+        ConstraintLayout constraintLayout = new ConstraintLayout(this);
         TextView textView = new TextView(this);
-
         textView.setText("Hello Android!");
+        textView.setTextSize(26);
 
-        textView.setTextSize(22);
+        ConstraintLayout.LayoutParams layoutParams = new ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.WRAP_CONTENT,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT);
 
-        setContentView(textView);
+        layoutParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
+        layoutParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
+        layoutParams.rightToRight = ConstraintLayout.LayoutParams.PARENT_ID;
+
+        textView.setLayoutParams(layoutParams);
+
+        constraintLayout.addView(textView);
+
+        setContentView(constraintLayout);
     }
 }
