df490a86a7fbd54283aa5abf3571014628f70ff8
bystrovdev <bystrovdev@mail.ru>
2023-01-10 13:35:18 +0400

    We have learned All View and started layout

--- app/src/main/java/ru/synergy/myapplication/MainActivity.java
+++ app/src/main/java/ru/synergy/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package ru.synergy.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -13,7 +14,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.second_layout);
+        setContentView(R.layout.activity_main);
+
+//        TextView textView = (TextView) findViewById(R.id.normal);
+//
+//        textView.setText("Text from Java code");
+//        textView.setTextColor(Color.GREEN);
 
 
 //        TextView textView = (TextView) findViewById(R.id.header);
