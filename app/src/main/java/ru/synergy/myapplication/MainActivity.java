171c9d7893fc5dc8e728197f7f499c9d53d6a34d
bystrovdev <bystrovdev@mail.ru>
2023-01-12 12:12:06 +0400

    Testing on the second screen

--- app/src/main/java/ru/synergy/myapplication/MainActivity.java
+++ app/src/main/java/ru/synergy/myapplication/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.second_layout);
 
 //        TextView textView = (TextView) findViewById(R.id.normal);
 //
