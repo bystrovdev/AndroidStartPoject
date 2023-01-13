6da8fbdcf8ae86b5304e24858b24856126bcbc69
bystrovdev <bystrovdev@mail.ru>
2023-01-13 12:25:58 +0400

    We first calculate done

--- app/src/main/java/ru/synergy/myapplication/Calculator.java
+++ app/src/main/java/ru/synergy/myapplication/Calculator.java
@@ -3,6 +3,12 @@ package ru.synergy.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Calculator extends AppCompatActivity {
 
@@ -10,5 +16,51 @@ public class Calculator extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
+        final Button calculate = (Button) findViewById(R.id.calc);
+
+        calculate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calculateAnswer();
+            }
+        });
+    }
+
+    private void calculateAnswer() {
+        EditText numOne = (EditText) findViewById(R.id.editTextNumberDecimal);
+        EditText numTwo = (EditText) findViewById(R.id.editTextNumberDecimal2);
+
+        RadioButton add = (RadioButton) findViewById(R.id.add);
+        RadioButton sub = (RadioButton) findViewById(R.id.subtract);
+        RadioButton multiply = (RadioButton) findViewById(R.id.multiple);
+        RadioButton divide = (RadioButton) findViewById(R.id.divide);
+
+        TextView answer = (TextView) findViewById(R.id.result);
+
+        float numberOne = Integer.parseInt(numOne.getText().toString());
+        float numberTwo = Integer.parseInt(numTwo.getText().toString());
+
+        float solution = 0;
+
+        if(add.isChecked()) {
+            solution = numberOne + numberTwo;
+        }
+        if(sub.isChecked()) {
+            solution = numberOne - numberTwo;
+        }
+        if(multiply.isChecked()) {
+            solution = numberOne * numberTwo;
+        }
+        if(divide.isChecked()) {
+            if(numberTwo == 0) {
+                Toast.makeText(this, "Number two cannot be zero!", Toast.LENGTH_LONG).show();
+                return;
+            }
+            solution = numberOne / numberTwo;
+        }
+
+        answer.setText("The answer is " + solution);
+
     }
 }
