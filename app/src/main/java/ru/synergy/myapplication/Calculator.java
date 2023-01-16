72e62509b8b560d4d25a19baaa15eafedd02567d
Bystrov Yaroslav <121603219+bystrovdev@users.noreply.github.com>
2023-01-16 19:35:31 +0400

    Revert "Intent practice"

--- app/src/main/java/ru/synergy/myapplication/Calculator.java
+++ app/src/main/java/ru/synergy/myapplication/Calculator.java
@@ -1,15 +1,11 @@
 package ru.synergy.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cursoradapter.widget.SimpleCursorAdapter;
 
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,30 +19,10 @@ public class Calculator extends AppCompatActivity {
 
         final Button calculate = (Button) findViewById(R.id.calc);
 
-
-//        // Context training
-//        TextView textView = new TextView();
-//        ListView adapter = new SimpleCursorAdapter(getApplicationContext());
-//
-//        // Доступ из класса Activity -- наследник Context
-//        getSystemService(LAYOUT_INFLATER_SERVICE);
-//
-//        // Shared prefs доступ c использованием контекста приложения
-//        SharedPreferences prefs = getApplicationContext().getSharedPreferences("PREFS", MODE_PRIVATE);
-
-
-        ////
-
-
-        //// intent - посылка
-
-
         calculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 calculateAnswer();
-                Intent i = new Intent(Calculator.this, MainActivity.class); // Написать письмо
-                startActivity(i);  // Отправить письмо
             }
         });
     }
@@ -59,10 +35,6 @@ public class Calculator extends AppCompatActivity {
         RadioButton sub = (RadioButton) findViewById(R.id.subtract);
         RadioButton multiply = (RadioButton) findViewById(R.id.multiple);
         RadioButton divide = (RadioButton) findViewById(R.id.divide);
-
-        numOne.setText("0");
-        numTwo.setText("0");
-        add.setChecked(true);
 
         TextView answer = (TextView) findViewById(R.id.result);
 
